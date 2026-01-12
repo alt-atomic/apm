3b26ccfe4be9aa7e95877d6a32b691a0af34ca47
Vladimir Romanov <rirusha@altlinux.org>
2026-01-12 23:41:59 +0300

    apt-bridge: Add translation and update update_pot script

--- data/apt-bridge/update-apm.lua
+++ data/apt-bridge/update-apm.lua
@@ -1,2 +1,10 @@
-print("Updating apm database")
+local gettext_handle = io.popen("gettext apm 'Updating apm database' 2>/dev/null")
+local message = gettext_handle:read("*l")
+gettext_handle:close()
+
+if not message or message == "" then
+    message = "Updating apm database"
+end
+
+print(message)
 os.execute("apm s update")
--- po/update_pot.sh
+++ po/update_pot.sh
@@ -4,3 +4,11 @@
 sh po/update_potfiles.sh
 
 cat ./po/POTFILES | xargs xgettext --language=C --keyword=T_ --keyword=TN_:1,2 --keyword=TD_:2 --keyword=TC_:1c,2 -o po/apm.pot --from-code=UTF-8 --add-comments --package-name=apm
+
+# TODO: Add autogeneration
+cat << EOF >> po/apm.pot
+
+#: data/apt-bridge/update-apm.lua:6
+msgid "Updating apm database"
+msgstr ""
+EOF
