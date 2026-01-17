4ad27362983a4b9057fe44a0b212aa3f79a0b19b
Vladimir Romanov <rirusha@altlinux.org>
2026-01-17 10:35:27 +0300

    feat: Replace hardcoded array with generated array from modules map keys

--- internal/common/build/core/models.go
+++ internal/common/build/core/models.go
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2026 Vladimir Romanov <rirusha@altlinux.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see
+ * <https://www.gnu.org/licenses/gpl-3.0-standalone.html>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 package core
 
 import (
@@ -62,12 +81,12 @@ var ModelMap = map[string]func() models.Body{
 
 // GetAllModuleTypes возвращает список всех типов модулей
 func GetAllModuleTypes() []string {
-	return []string{
-		TypeBranding, TypeCopy, TypeGit, TypeInclude,
-		TypeKernel, TypeLink, TypeMerge, TypeMkdir,
-		TypeMove, TypeNetwork, TypePackages, TypeRemove,
-		TypeReplace, TypeRepos, TypeShell, TypeSystemd,
+	keys := make([]string, 0, len(ModelMap))
+	for k := range ModelMap {
+		keys = append(keys, k)
 	}
+
+	return keys
 }
 
 var (
