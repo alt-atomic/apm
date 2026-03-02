cecd2e0d1ace4c6995aa6281c3552f58ffddbdae
Dmitry Udalov <udalov@altlinux.org>
2026-03-03 01:30:15 +0600

    fix: set full version for swagger
    Ref #69

--- internal/common/http_server/server.go
+++ internal/common/http_server/server.go
@@ -400,11 +400,11 @@ func (s *Server) RegisterOpenAPIFromRegistry(gen OpenAPIFromRegistry) {
     <title>APM API Documentation</title>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@5.32.0/swagger-ui.css">
 </head>
 <body>
     <div id="swagger-ui"></div>
-    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+    <script src="https://unpkg.com/swagger-ui-dist@5.32.0/swagger-ui-bundle.js"></script>
     <script>
         window.onload = function() {
             SwaggerUIBundle({
